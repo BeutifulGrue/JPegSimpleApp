c3dd80ab4b36d422b2d98301094040b964f1b082
Garth T Upshaw <garth@open-mesh.com>
2018-12-03 12:21:18 -0800

    WIP

--- app/src/main/java/com/garthskidstuff/jpegsimpleapp/MainActivity.java
+++ app/src/main/java/com/garthskidstuff/jpegsimpleapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.garthskidstuff.jpegsimpleapp;
 
+import android.app.LauncherActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,15 +26,21 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private static final String TAG = "MainActivity";
+    private View mainView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainView = findViewById(R.id.)
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -64,12 +78,21 @@ public class MainActivity extends AppCompatActivity
                 try () {
                     File a = new File("A.jpg");
                     File b = new File("B.jpg");
-
+                    ArrayList<File> files = new ArrayList();
+                    if (a.exists())
+                    {
+                        files.add(a);
+                    }
+                    if (b.exists())
+                    {
+                        files.add(b);
+                    }
                     if (ab.exists())
                     {
-                        
-
+                        files.add(ab);
                     }
+
+                    share(files);
                 }
             }
         });
@@ -98,5 +121,63 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void share(final List<File> files)
+    {
+        final int num = files.size();
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.share));
+        StringBuilder sb = new StringBuilder();
+        for(File file : files)
+        {
+            sb.append(file.getName() + " ");
+        }
+        sb.append(": OK?");
+        builder.setMessage(sb.toString());
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                if (!files.isEmpty())
+                {
+                    final ArrayList<Uri> imageUris = new ArrayList<>();
+                    for (File file : files)
+                    {
+                        imageUris.add(Uri.fromFile(file));
+                    }
+
+                    try
+                    {
+                        MediaScannerConnection
+                                .scanFile(MainActivity.this, new String[]{imageUris.toString()},
+                                          null, new MediaScannerConnection.OnScanCompletedListener()
+                                        {
+                                            public void onScanCompleted(String path, Uri uri)
+                                            {
+                                                Intent shareIntent = new Intent();
+                                                shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+                                                shareIntent.putParcelableArrayListExtra(
+                                                        Intent.EXTRA_STREAM, imageUris);
+                                                shareIntent.setType("*/*");
+                                                MainActivity.this.startActivity(
+                                                        Intent.createChooser(shareIntent,
+                                                                "share files to"));
+                                            }
+                                        });
+                        String msg = "Shared " ++ files.size() ++ "file(s)?";
+                        Snackbar.make(mainView, msg, Snackbar.LENGTH_LONG);
+                    }
+                    catch (Exception e)
+                    {
+                        Log.e(TAG, "Upload Error:" + e);
+                    }
+                }
+            }
+        });
+        builder.show();
     }
 }
